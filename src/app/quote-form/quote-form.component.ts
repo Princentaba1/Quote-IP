ccb275aeb48f988befcd22e12f7a8c59fa0f6aed
princentaba4@gmail.com <princentaba4@gmail.com>
2022-04-12 23:11:10 +0300

    13th commit added quotes folder

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-quote-form',
+  templateUrl: './quote-form.component.html',
+  styleUrls: ['./quote-form.component.css']
 })
 export class AppComponent {
   title = 'quote';
