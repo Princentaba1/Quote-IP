8fe67748b99de50033b3a7c08cb6e0a0d9c4379c
princentaba4@gmail.com <princentaba4@gmail.com>
2022-04-15 10:51:22 +0300

    19th commit

--- src/app/quote-form/quote-form.component.spec.ts
+++ src/app/quote-form/quote-form.component.spec.ts
@@ -1,35 +1,25 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { ComponentFixture,TestBed } from '@angular/core/testing';
 
-describe('AppComponent', () => {
+import { QuoteFormComponent } from './quote-form.component';
+
+describe('QuoteFormComponent', () => {
+  let Component: QuoteFormComponent;
+  let fixture: ComponentFixture<QuoteFormComponent>;
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+      declarations: [ QuoteFormComponent]
+    })
+    .compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'quote'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('quote');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  beforeEach(() => {
+    fixture =TestBed.createComponent(QuoteFormComponent);
+    Component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('quote app is running!');
-  });
+    });
+
+    it('should create', () => {
+      expect(Component).toBeTruthy();
+    });
 });
--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quotes } from './quote-detail.component.spec';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-quote-form',
+  templateUrl: './quote-form.component.html',
+  styleUrls: ['./quote-form.component.css']
 })
-export class AppComponent {
-  title = 'quote';
+export class QuoteFormComponent implements OnInit {
+
+  newQuote = new Quotes(0,"","","",new Date(),0,0);
+  @Output() addQuote= new EventEmitter<Quotes>();
+
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
+  }
+
+
+  // const nameQuote = document.querySelector("textarea");
+
+  // onTextChange(value:any)
+  // {
+    
+  //   if(this.nameQuote.f(value) <= 0;)
+  //   {
+  //     alert("Textbox is empty !!!");
+  //   }
+    
+  // }
+
+  ngOnInit(): void {
+  }
+
 }
